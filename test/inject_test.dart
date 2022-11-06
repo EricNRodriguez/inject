01ee4439e31fc560a56b36a1526195faafd1e918
EricNRodriguez <rodriguez.eric.n.work@gmail.com>
2022-11-06 17:40:01 +1100

    initial basic tests

--- test/inject_test.dart
+++ test/inject_test.dart
@@ -1,99 +1,171 @@
+import 'dart:math';
+
 import 'package:inject/inject.dart';
+import 'package:inject/src/error.dart';
 import 'package:test/test.dart';
 
-class A {
+class ObjWithDeps {
   final String name;
+  final ObjWithDeps? dep1;
+  final ObjWithDeps? dep2;
 
-  static A buildWithEric(DependencyContainer container) {
-    return A(container<String>(key: "eric"));
-  }
-
-  static A buildWithCarl(DependencyContainer container) {
-    return A(container<String>(key: "carl"));
-  }
-
-  static A buildWithNone(DependencyContainer container) {
-    return A("none:((");
-  }
-
-  A(this.name);
+  ObjWithDeps(this.name, this.dep1, this.dep2);
 
   @override
-  String toString() {
-    return "A\n\t\t\t$name";
-  }
-}
-
-class B {
-  final A a;
-
-  static B build(DependencyContainer container) {
-    return B(container<A>());
-  }
-
-  B(this.a);
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is ObjWithDeps && runtimeType == other.runtimeType &&
+              name == other.name && dep1 == other.dep1 && dep2 == other.dep2;
 
   @override
-  String toString() {
-    return "B\n\t\t$a";
-  }
-}
-
-class C {
-  final A? a;
-  final B? b;
-
-  static C buildWithCarlA(DependencyContainer container) {
-    return C(container<A>(key: "carl"), container<B>());
-  }
-
-  static C buildWithEricA(DependencyContainer container) {
-    return C(container<A>(key: "eric"), container<B>());
-  }
-
-  static Future<C> buildAsync(DependencyContainer container) {
-    return Future.value(C(container<A>(), container<B>()));
-  }
-
-  C(this.a, this.b);
-
-  @override
-  String toString() {
-    return "C\n\t$a\n\t$b\n";
-  }
+  int get hashCode => name.hashCode ^ dep1.hashCode ^ dep2.hashCode;
 }
 
 void main() {
-  test('duplicate singletons', () {
-    DependencyContainerBuilder builder = DependencyContainerBuilder()
-      ..withSingleton<String>("HI", key: "greeting")
-      ..withFactory((container) => "HI", key: "greetin2g");
+  test('correctly constructs simple object graph', () {
+    ObjWithDeps buildA(DependencyContainer container) {
+      return ObjWithDeps(
+          "aye",
+          container<ObjWithDeps>(key: "B"),
+          container<ObjWithDeps>(key: "C"),
+      );
+    }
 
-    DependencyContainer container = builder.build();
+    ObjWithDeps buildB(DependencyContainer container) {
+      return ObjWithDeps(
+        "bee",
+        container<ObjWithDeps>(key: "C"),
+        container<ObjWithDeps>(key: "D"),
+      );
+    }
+
+    ObjWithDeps buildC(DependencyContainer container) {
+      return ObjWithDeps(
+        "see",
+        container<ObjWithDeps>(key: "D"),
+        null,
+      );
+    }
+
+    ObjWithDeps buildD(DependencyContainer container) {
+      return ObjWithDeps(
+          "dee",
+          null,
+          null,
+      );
+    }
+
+    DependencyContainerBuilder containerBuilder = DependencyContainerBuilder()
+      ..withFactory(buildA, key: "A")
+      ..withFactory(buildB, key: "B")
+      ..withFactory(buildC, key: "C")
+      ..withFactory(buildD, key: "D");
+
+    DependencyContainer container = containerBuilder.build();
+
+    ObjWithDeps expectedObjectGraph = ObjWithDeps(
+      "aye",
+      ObjWithDeps(
+        "bee",
+        ObjWithDeps(
+          "see",
+          ObjWithDeps(
+            "dee",
+            null,
+            null,
+          ),
+          null,
+        ),
+        ObjWithDeps(
+          "dee",
+          null,
+          null,
+        )
+      ),
+      ObjWithDeps(
+        "see",
+          ObjWithDeps(
+            "dee",
+            null,
+            null,
+          ),
+        null,
+      )
+    );
+
+    ObjWithDeps constructedObjectGraph = container<ObjWithDeps>(key: "A");
+
+    expect(constructedObjectGraph, equals(expectedObjectGraph));
   });
-  test('throwaway', () async {
-    DependencyContainerBuilder builder = DependencyContainerBuilder()
-      ..withSingleton<String>("eric!!!!!!!", key: "eric")
-      ..withFactory<String>((_) => "carl!!!!!!!", key: "carl")
-      ..withFactory<C>(C.buildWithCarlA, key: "carl")
-      ..withFactory<C>(C.buildWithEricA, key: "eric")
-      ..withFactory<Future<C>>(C.buildAsync)
-      ..withFactory<B>(B.build)
-      ..withFactory<A>(A.buildWithCarl, key: "carl")
-      ..withSingleton<A>(A("eric!!!!!!!"), key: "eric")
-      ..withSingleton<A>(A("eric!!!!!!!"), key: "erizzzzc")
-      ..withFactory((container) => A("eric!!!!!!!2"));
 
-    DependencyContainer container = builder.build();
+  test('only constructs singletons a single time', () {
+    ObjWithDeps buildA(DependencyContainer container) {
+      return ObjWithDeps(
+        "aye",
+        container<ObjWithDeps>(key: "B"),
+        container<ObjWithDeps>(key: "B"),
+      );
+    }
 
-    C c1 = container<C>(key: "carl");
-    C c2 = container<C>(key: "carl");
+    ObjWithDeps buildB(DependencyContainer container) {
+      return ObjWithDeps(
+        "bee",
+        null,
+        null,
+      );
+    }
 
-    print(c1.a == c2.a);
+    DependencyContainerBuilder containerBuilder = DependencyContainerBuilder()
+      ..withFactory(buildA, key: "A")
+      ..withLazySingleton(buildB, key: "B");
 
-    C c3 = container<C>(key: "eric");
-    C c4 = container<C>(key: "eric");
+    DependencyContainer container = containerBuilder.build();
 
-    print(c3.a == c4.a);
+    ObjWithDeps constructedObj = container<ObjWithDeps>(key: "A");
+
+    expect(constructedObj.dep1, isNotNull);
+    expect(constructedObj.dep1, equals(constructedObj.dep2));
+  });
+
+  test('throws when cycle is detected', () {
+    ObjWithDeps buildA(DependencyContainer container) {
+      return ObjWithDeps(
+        "aye",
+        container<ObjWithDeps>(key: "B"),
+        container<ObjWithDeps>(key: "B"),
+      );
+    }
+
+    ObjWithDeps buildB(DependencyContainer container) {
+      return ObjWithDeps(
+        "bee",
+        container<ObjWithDeps>(key: "C"),
+        null,
+      );
+    }
+
+    ObjWithDeps buildC(DependencyContainer container) {
+      return ObjWithDeps(
+        "see",
+        container<ObjWithDeps>(key: "B"),
+        null,
+      );
+    }
+
+
+    DependencyContainerBuilder containerBuilder = DependencyContainerBuilder()
+      ..withFactory(buildA, key: "A")
+      ..withFactory(buildB, key: "B")
+      ..withFactory(buildC, key: "C");
+
+    DependencyContainer container = containerBuilder.build();
+
+    expect(() => container<ObjWithDeps>(key: "A"), throwsCircularDependencyError);
+    expect(() => container<ObjWithDeps>(key: "B"), throwsCircularDependencyError);
+    expect(() => container<ObjWithDeps>(key: "C"), throwsCircularDependencyError);
   });
 }
+
+const isCircularDependencyError = TypeMatcher<CircularDependencyError>();
+const Matcher throwsCircularDependencyError = Throws(isCircularDependencyError);
+
