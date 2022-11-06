17b40b8261721cfa59ca7e0111698b2c3db83f9a
Eric <43801271+EricNRodriguez@users.noreply.github.com>
2022-11-06 13:00:06 +1100

    Initial commit

---
+++ example/inject_example.dart
@@ -0,0 +1,6 @@
+import 'package:inject/inject.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/inject.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library inject;
+
+export 'src/inject_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
